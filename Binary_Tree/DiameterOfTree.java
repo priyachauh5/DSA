a0baf99fa274cf254347dbd5ceb433b364c4c48f
Delta Student <151124074+priyachauh5@users.noreply.github.com>
2025-01-22 23:37:16 +0530

    Binary_Tree

--- Binary_Tree/DiameterOfTree.java
+++ Binary_Tree/DiameterOfTree.java
@@ -20,7 +20,42 @@ public class TreeHeight{
         return Math.max(lh, rh)+1;
     }
 
-    public static void main(String args[]){
+    public static int diameter2(Node root){ //O(n^2)
+        if(root==null){
+            return 0;
+        }
+        int leftDiam=diameter2(root.left);
+        int leftHt=height(root.left);
+        int rightDiam=diameter2(root.right);
+        int rightHt=height(root.right);
+
+        int selfDiam=leftHt+rightHt+1;
+
+        return Math.max(selfDiam, Math.max(leftDiam, rightDiam));
+    }
+
+    static class Info{
+        int diam;
+        int ht;
+        public Info(int diam, int ht){
+            this.diam=diam;
+            this.ht=ht;
+        }
+    }
+    public static Info diameter(Node root){ //O(n)
+        if(root==null){
+            return new Info(0,0);
+        }
+        Info leftInfo=diameter(root.left);
+        Info rightInfo=diameter(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1); 
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+        
+        return new Info(diam, ht);
+    }
+
+     public static void main(String args[]){
         /*
             1
            / \
@@ -41,6 +76,9 @@ public class TreeHeight{
        root.right.right.left=new Node(8);
        root.right.right.right=new Node(9);
        
-       System.out.println(height(root));
+       System.out.println(diameter(root).diam);
     }
 }
+
+
+   
