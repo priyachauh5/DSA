82953b47493f829e4740c2b7ae5963d9de642061
Priya Chauhan <priyachauh5@gmail.com>
2025-05-06 18:46:36 +0530

    Graphs

--- Graphs/Bipartite_Graph.java
+++ Graphs/Bipartite_Graph.java
@@ -9,6 +9,7 @@ public class Cycle_Detection{
             this.dest=d;
         }
     }
+    
     static void createGraph(ArrayList<Edge>[] graph){
         for(int i=0; i<graph.length; i++){
             graph[i]=new ArrayList<>();
