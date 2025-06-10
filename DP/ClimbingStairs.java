b2a7e4cc0cdda5fa5575ce2b077d833f6b54d44c
Priya Chauhan <priyachauh5@gmail.com>
2025-06-10 18:29:11 +0530

    DP

---
+++ DP/ClimbingStairs.java
@@ -0,0 +1,57 @@
+package DSA.DP;
+import java.util.Arrays;
+//time complexity: O(2^n)
+// public class ClimbingStairs {
+//     public static int countWays(int n){
+//         if(n==0) return 1;
+//         if(n<0) return 0;
+//         return countWays(n-1)+countWays(n-2);
+//     }
+//     public static void main(String args[]){
+//         int n=5; //no of stairs;
+//         System.out.println(countWays(n));
+//     }
+// }
+
+// //time complexity: O(n)
+//memoization
+// public class ClimbingStairs {
+//     public static int countWays(int n){
+//         int ways[]=new int[n+1];
+//         Arrays.fill(ways, -1);
+//         if(n==0) return 1;
+//         if(n<0) return 0;
+//         if(ways[n]!=-1){
+//             return ways[n];
+//         }
+//         ways[n]= countWays(n-1)+countWays(n-2);
+//         return ways[n];
+//     }
+//     public static void main(String args[]){
+//         int n=5; //no of stairs;
+//         System.out.println(countWays(n));
+//     }
+// }
+
+
+//time complexity: O(n)
+//Tabulation
+public class ClimbingStairs {
+    public static int countWays(int n){
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        //tabulation loop
+        for(int i=1; i<=n; i++){
+            if(i==1){
+                dp[i]=dp[i-1]+0;
+            }else{
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+    public static void main(String args[]){
+        int n=5; //no of stairs;
+        System.out.println(countWays(n));
+    }
+}
---
+++ DP/fibonacciRecursion.java
@@ -0,0 +1,57 @@
+package DSA.DP;
+
+// Time complexity: O(2^n)
+//Recusion
+
+// public class fibonacciRecursion {
+//     public static int fib(int n){
+//         if(n==1 || n==0){
+//             return n;
+//         }
+//         return fib(n-1)+fib(n-2);
+//     }
+//     public static void main(String args[]){
+//         int n=6;
+//         System.out.println(fib(n));
+//     }
+// }
+
+//Advance recursion 
+//Time Complexity: O(n)
+//Memoization
+
+// public class fibonacciRecursion {
+//     public static int fib(int n, int f[]){
+//         if(n==1 || n==0){
+//             return n;
+//         }
+//         if(f[n]!=0){
+//             return f[n];
+//         }
+//         f[n]=fib(n-1, f)+fib(n-2, f);
+//         return f[n];
+//     }
+//     public static void main(String args[]){
+//         int n=6;
+//         int f[]=new int[n+1];
+//         System.out.println(fib(n,f));
+//     }
+// }
+
+//Tabulation
+//O(n)
+public class fibonacciRecursion {
+    public static int fibTabulation(int n){
+       int dp[]=new int[n+1];
+       dp[0]=0;
+       dp[1]=1;
+       for(int i=2; i<=n; i++){
+        dp[i]=dp[i-1]+dp[i-2];
+       }
+       return dp[n];
+    }
+    public static void main(String args[]){
+        int n=6;
+        System.out.println(fibTabulation(n));
+    }
+}
